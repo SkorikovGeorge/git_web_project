e46e4f2dc4c33d1ebb9120d67b9be4c73c3d7a42
Alena <azabuselova@gmail.com>
2022-04-23 22:10:04 +0300

    база данных, две таблицы юзер и инфо

---
+++ website/models.py
@@ -0,0 +1,16 @@
+from flask_login import UserMixin
+from . import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    notes = db.relationship('Info')
+
+
+class Info(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
