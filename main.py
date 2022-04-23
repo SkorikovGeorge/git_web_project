3332118336c5f0c463f7b5829a21cea34fde9196
Георгий <skorikovgk@yandex.ru>
2022-04-23 15:32:18 +0300

    Первые страницы сайта, пути к ним

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-from flask import Flask
+from website import create_app
 
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return '<h1> First commit <h1>'
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
