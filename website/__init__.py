8d162538485dc3b781f07c846ce3bca0d5a57a28
Alena <azabuselova@gmail.com>
2022-04-23 23:11:14 +0300

    база данных, две таблицы юзер и инфо

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
 
 
 db = SQLAlchemy()
@@ -8,7 +9,8 @@ db_name = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hbhjjjbbcjbjababuafvbaubjs'
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_name}'
     db.init_app(app)
 
@@ -18,5 +20,13 @@ def create_app():
     app.register_blueprint(routes, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Info
+
+    create_database(app)
+
     return app
 
+
+def create_database(app):
+    if not path.exists('website/' + db_name):
+        db.create_all(app=app)
