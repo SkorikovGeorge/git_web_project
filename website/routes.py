c7f2fdad691188cd24f1490e3f0b0f7175f5a75f
Alena <azabuselova@gmail.com>
2022-04-24 11:38:21 +0300

    добавление текста в бд

--- website/routes.py
+++ website/routes.py
@@ -1,9 +1,25 @@
-from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+from .models import Info
+
 
 routes = Blueprint('routes', __name__)
 
 
-@routes.route('/')
+@routes.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
+    if request.method == 'POST':
+        user_text = request.form.get('text')
+
+        if len(user_text) < 1:
+            flash('Text is too short!', category='error')
+        else:
+            new_text = Info(user_id=current_user.id)
+            new_text.info = user_text
+            db.session.add(new_text)
+            db.session.commit()
+            flash('Info added!', category='success')
+
     return render_template("home.html", user=current_user)
