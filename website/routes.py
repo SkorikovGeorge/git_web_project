7c4c2b6f5dc5394cb70722230c64160c1200794e
Alena <azabuselova@gmail.com>
2022-04-24 15:58:55 +0300

    видимость кнопок

--- website/routes.py
+++ website/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 
 routes = Blueprint('routes', __name__)
@@ -6,4 +7,4 @@ routes = Blueprint('routes', __name__)
 
 @routes.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
