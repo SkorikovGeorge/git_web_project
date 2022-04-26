baca36f19bb6103fc20676ec0a1e6bae61b451ab
Георгий <skorikovgk@yandex.ru>
2022-04-26 14:49:24 +0300

    Добавил поле поиска заметки, вписываю полученное в переменную

--- website/routes.py
+++ website/routes.py
@@ -11,15 +11,18 @@ routes = Blueprint('routes', __name__)
 def home():
     if request.method == 'POST':
         user_text = request.form.get('info')
-
-        if len(user_text) < 1:
-            flash('Текст слишком короткий, попробуйте ещё раз', category='error')
+        find_text = request.form.get('find')
+        if find_text:
+            print(find_text)
         else:
-            new_text = Info(user_id=current_user.id)
-            new_text.info = user_text
-            db.session.add(new_text)
-            db.session.commit()
-            flash('Текст добавлен', category='success')
+            if len(user_text) < 1:
+                flash('Текст слишком короткий, попробуйте ещё раз', category='error')
+            else:
+                new_text = Info(user_id=current_user.id)
+                new_text.info = user_text
+                db.session.add(new_text)
+                db.session.commit()
+                flash('Текст добавлен', category='success')
 
     return render_template("home.html", user=current_user)
 
